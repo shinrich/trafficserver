f92c7cc1c02b36c4daddf36d844d46d0b3d263fa
Sudheer Vinukonda <sudheerv@yahoo-inc.com>
2015-04-29 16:41:01 +0000

    [Bug 7036878]: default proxy.config.net.default_inactivity_timeout to 300 sec

--- mgmt/RecordsConfig.cc
+++ mgmt/RecordsConfig.cc
@@ -817,7 +817,7 @@ static const RecordElement RecordsConfig[] =
   ,
   {RECT_CONFIG, "proxy.config.net.poll_timeout", RECD_INT, "10", RECU_NULL, RR_NULL, RECC_NULL, NULL, RECA_NULL}
   ,
-  {RECT_CONFIG, "proxy.config.net.default_inactivity_timeout", RECD_INT, "86400", RECU_DYNAMIC, RR_NULL, RECC_NULL, NULL, RECA_NULL}
+  {RECT_CONFIG, "proxy.config.net.default_inactivity_timeout", RECD_INT, "300", RECU_DYNAMIC, RR_NULL, RECC_NULL, NULL, RECA_NULL}
   ,
 
   //##############################################################################
