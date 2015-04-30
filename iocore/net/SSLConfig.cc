bd32d6aeb733c3907fb4b5ab8a752ca1f1fdac15
Sudheer Vinukonda <sudheerv@yahoo-inc.com>
2015-04-30 16:57:06 +0000

    [TS-3016]: configure proxy.config.ssl.disable_single_dh_use to turn off single dh key generation (local merge)

--- iocore/net/SSLConfig.cc
+++ iocore/net/SSLConfig.cc
@@ -207,14 +207,17 @@ SSLConfigParams::initialize()
 #endif
   }
 
-// Enable ephemeral DH parameters for the case where we use a cipher with DH forward security.
+  // Enable ephemeral DH parameters for the case where we use a cipher with DH forward security.
+  REC_ReadConfigInteger(options, "proxy.config.ssl.disable_single_dh_use");
+  if (!options) {
 #ifdef SSL_OP_SINGLE_DH_USE
-  ssl_ctx_options |= SSL_OP_SINGLE_DH_USE;
+    ssl_ctx_options |= SSL_OP_SINGLE_DH_USE;
 #endif
 
 #ifdef SSL_OP_SINGLE_ECDH_USE
-  ssl_ctx_options |= SSL_OP_SINGLE_ECDH_USE;
+    ssl_ctx_options |= SSL_OP_SINGLE_ECDH_USE;
 #endif
+  }
 
   // Enable all SSL compatibility workarounds.
   ssl_ctx_options |= SSL_OP_ALL;
