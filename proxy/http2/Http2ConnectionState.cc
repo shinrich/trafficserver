7012239a8da0278604159a02777492daa3bc7430
Masakazu Kitajo <maskit@apache.org>
2017-04-19 10:14:48 +0900

    Fixes padding byte being part of the POST/PUT data (2nd try)
    (cherry picked from commit 74bbc781382146c9d87683b8c0ebc537ee40cdee)

--- proxy/http2/Http2ConnectionState.cc
+++ proxy/http2/Http2ConnectionState.cc
@@ -93,7 +93,7 @@ rcv_data_frame(Http2ClientSession &cs, Http2ConnectionState &cstate, const Http2
   }
 
   if (frame.header().flags & HTTP2_FLAGS_DATA_PADDED) {
-    frame.reader()->read(&pad_length, HTTP2_DATA_PADLEN_LEN);
+    frame.reader()->memcpy(&pad_length, HTTP2_DATA_PADLEN_LEN, nbytes);
     nbytes += HTTP2_DATA_PADLEN_LEN;
     if (pad_length > payload_length) {
       // If the length of the padding is the length of the
@@ -136,6 +136,10 @@ rcv_data_frame(Http2ClientSession &cs, Http2ConnectionState &cstate, const Http2
   // update its offset via consume.  Otherwise, we will read the same data on the
   // second time through
   IOBufferReader *myreader = frame.reader()->clone();
+  // Skip pad length field
+  if (frame.header().flags & HTTP2_FLAGS_DATA_PADDED) {
+    myreader->consume(HTTP2_DATA_PADLEN_LEN);
+  }
   while (nbytes < payload_length - pad_length) {
     size_t read_len = sizeof(buf);
     if (nbytes + read_len > unpadded_length)
