8aac52d191f6d24bcfe3ad514b5e9566f15b7097
Alan M. Carroll <amc@apache.org>
2017-05-30 11:31:54 -0500

    Change locking for spawning dedicated threads to avoid race conditions.
    (cherry picked from commit ed8527a501a679176394cd193ab8b482bf57e942)

--- iocore/eventsystem/I_EventProcessor.h
+++ iocore/eventsystem/I_EventProcessor.h
@@ -317,6 +317,7 @@ public:
   EThread *all_dthreads[MAX_EVENT_THREADS];
   int n_dthreads; // No. of dedicated threads
   volatile int thread_data_used;
+  ink_mutex dedicated_spawn_thread_mutex;
 };
 
 extern inkcoreapi class EventProcessor eventProcessor;
