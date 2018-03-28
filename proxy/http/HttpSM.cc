5ec6c5e91d555b3cedce759afed12205b2d844b2
Alan M. Carroll <amc@apache.org>
2018-03-28 13:05:36 -0500

    Fix per origin connection throttling to include the current count and limit.
    (cherry picked from commit 6c6f6842172e9a8218a53c2aa6c467f9e0750ac6)

--- proxy/http/HttpSM.cc
+++ proxy/http/HttpSM.cc
@@ -5048,17 +5048,18 @@ HttpSM::do_http_server_open(bool raw)
     ConnectionCount *connections = ConnectionCount::getInstance();
 
     INK_MD5 hostname_hash;
-    MD5Context md5_ctx;
-    md5_ctx.hash_immediate(hostname_hash, static_cast<const void *>(t_state.current.server->name),
+    MD5Context().hash_immediate(hostname_hash, static_cast<const void *>(t_state.current.server->name),
                            strlen(t_state.current.server->name));
 
-    if (connections->getCount(t_state.current.server->dst_addr, hostname_hash,
-                              (TSServerSessionSharingMatchType)t_state.txn_conf->server_session_sharing_match) >=
-        t_state.txn_conf->origin_max_connections) {
+    auto ccount = connections->getCount(t_state.current.server->dst_addr, hostname_hash,
+                                        (TSServerSessionSharingMatchType)t_state.txn_conf->server_session_sharing_match);
+    if (ccount >= t_state.txn_conf->origin_max_connections) {
       ip_port_text_buffer addrbuf;
       ats_ip_nptop(&t_state.current.server->dst_addr.sa, addrbuf, sizeof(addrbuf));
-      DebugSM("http", "[%" PRId64 "] over the number of connection for this host: %s", sm_id, addrbuf);
-      Warning("[%" PRId64 "] over the max number of connections for this host: %s", sm_id, addrbuf);
+      DebugSM("http", "[%" PRId64 "] too many connections (%d) for this host (%" PRId64 "): %s", sm_id, ccount,
+              t_state.txn_conf->origin_max_connections, addrbuf);
+      Warning("[%" PRId64 "] too many connections (%d) for this host (%" PRId64 "): %s", sm_id, ccount,
+              t_state.txn_conf->origin_max_connections, addrbuf);
       ink_assert(pending_action == nullptr);
 
       // if we were previously queued, or the queue is disabled-- just reschedule
