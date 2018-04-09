c6d8ac34bbbcb15520e5f88b3bcd53af17e95b48
zeyuany <zeyuany@yahoo-inc.com>
2018-04-09 09:56:00 -0500

    Cherrypick b8e975223 (#750)
    Split txn and ssn user arg allocation

--- example/remap/remap.cc
+++ example/remap/remap.cc
@@ -299,7 +299,7 @@ TSRemapDoRemap(void *ih, TSHttpTxn rh, TSRemapRequestInfo *rri)
   }
 
   // How to store plugin private arguments inside Traffic Server request processing block.
-  if (TSHttpArgIndexReserve("remap_example", "Example remap plugin", &arg_index) == TS_SUCCESS) {
+  if (TSHttpTxnArgIndexReserve("remap_example", "Example remap plugin", &arg_index) == TS_SUCCESS) {
     fprintf(stderr, "[TSRemapDoRemap] Save processing counter %lu inside request processing block\n", _processing_counter);
     TSHttpTxnArgSet((TSHttpTxn)rh, arg_index, (void *)_processing_counter); // save counter
   }
