ff5c7cdb47a71f087363bdecf27ffc1cba8a503d
Jesse Zhang <macisasandwich@gmail.com>
2018-05-22 14:10:58 -0500

    Adding delay to fix race conditions (cherry picked from commit a03b86a0af312fe587815a67c795da66cb5f4d09)

--- tests/gold_tests/continuations/double.test.py
+++ tests/gold_tests/continuations/double.test.py
@@ -94,6 +94,7 @@ tr.Processes.Default.StartBefore(ssn1)
 tr.Processes.Default.StartBefore(ssn2)
 
 tr = Test.AddTestRun()
+tr.DelayStart = 10 # wait for stats to be updated
 tr.Processes.Default.Command = comparator_command.format('txn')
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Env = ts.Env
@@ -109,6 +110,7 @@ tr.Processes.Default.StartBefore(txn2)
 
 # session count is positive,
 tr = Test.AddTestRun()
+tr.DelayStart = 10 # wait for stats to be updated
 tr.Processes.Default.Command = "traffic_ctl metric get continuations_verify.ssn.close.1"
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Env = ts.Env
@@ -117,6 +119,7 @@ tr.StillRunningAfter = ts
 
 # and we receive the same number of transactions as we asked it to make
 tr = Test.AddTestRun()
+tr.DelayStart = 10 # wait for stats to be updated
 tr.Processes.Default.Command = "traffic_ctl metric get continuations_verify.txn.close.1"
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Env = ts.Env
