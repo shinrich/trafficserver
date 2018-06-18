40fb7c0ee0e8b1c82ec23422d7a590b9e67b93a3
Walter Karas <wkaras@oath.com>
2018-06-18 17:00:52 -0500

    Fix compilation error in 14724be5f22ee46eb56676eb4f5aaa476d3ef760 for std::atomic structure field initialization.

--- iocore/eventsystem/I_EventProcessor.h
+++ iocore/eventsystem/I_EventProcessor.h
@@ -290,10 +290,12 @@ public:
   struct ThreadGroupDescriptor {
     std::string _name;                               ///< Name for the thread group.
     int _count                = 0;                   ///< # of threads of this type.
-    std::atomic<int> _started = 0;                   ///< # of started threads of this type.
+    std::atomic<int> _started;                       ///< # of started threads of this type.
     int _next_round_robin     = 0;                   ///< Index of thread to use for events assigned to this group.
     Que(Event, link) _spawnQueue;                    ///< Events to dispatch when thread is spawned.
     EThread *_thread[MAX_THREADS_IN_EACH_TYPE] = {}; ///< The actual threads in this group.
+
+    ThreadGroupDescriptor() : _started(0) {}
   };
 
   /// Storage for per group data.
