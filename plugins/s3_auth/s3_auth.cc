feeffd587971d187acde64f8ab05caeee1c602a8
Fei Deng <duke8253@gmail.com>
2018-12-20 11:10:55 -0600

    use memcpy

--- plugins/s3_auth/s3_auth.cc
+++ plugins/s3_auth/s3_auth.cc
@@ -651,7 +651,7 @@ str_concat(char *dst, size_t dst_len, const char *src, size_t src_len)
   size_t to_copy = (src_len < dst_len) ? src_len : dst_len;
 
   if (to_copy > 0) {
-    (void)strncat(dst, src, to_copy);
+    memcpy(dst, src, to_copy);
   }
 
   return to_copy;
