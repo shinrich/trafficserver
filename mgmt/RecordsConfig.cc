5f13a4dfb4f71a834b1c0a5f814929151fd0b672
Susan Hinrichs <shinrich@oath.com>
2019-10-11 17:38:18 +0000

    Add setting to enable old remap plugin semantics
    (cherry picked from commit 2591b366818b9209c298e385d18f9a35b5272643)
    
    Conflicts:
    	mgmt/RecordsConfig.cc
    	proxy/http/remap/RemapProcessor.cc
    	proxy/http/remap/RemapProcessor.h

--- mgmt/RecordsConfig.cc
+++ mgmt/RecordsConfig.cc
@@ -1468,6 +1468,9 @@ static const RecordElement RecordsConfig[] =
   //#
   //###########
   {RECT_CONFIG, "proxy.config.http.host_sni_policy", RECD_INT, "2", RECU_NULL, RR_NULL, RECC_NULL, "[0-2]", RECA_NULL},
+
+  // Temporary control to add back old remap plugin execution behavior
+  {RECT_CONFIG, "proxy.config.http.remap.old_behavior", RECD_INT, "0", RECU_RESTART_TS, RR_NULL, RECC_NULL, "[0-1]", RECA_NULL},
 };
 // clang-format on
 
