39670f517d63cc8e23109cf8a028b01c5a87b042
Susan Hinrichs <shinrich@verizonmedia.com>
2020-05-01 19:35:54 +0000

    Improve client_vc tracking
    (cherry picked from commit 30311702b81eb960965aeba387a7b36fc1bb68b2)

--- proxy/http2/Http2Stream.cc
+++ proxy/http2/Http2Stream.cc
@@ -446,7 +446,7 @@ Http2Stream::initiating_close()
     bool sent_write_complete = false;
     if (_sm) {
       // Push out any last IO events
-      if (write_vio.cont) {
+      if (write_vio.cont && write_vio.nbytes > 0) {
         SCOPED_MUTEX_LOCK(lock, write_vio.mutex, this_ethread());
         // Are we done?
         if (write_vio.nbytes == write_vio.ndone) {
@@ -672,7 +672,8 @@ Http2Stream::signal_read_event(int event)
 void
 Http2Stream::signal_write_event(int event)
 {
-  if (this->write_vio.cont == nullptr || this->write_vio.cont->mutex == nullptr || this->write_vio.op == VIO::NONE) {
+  if (this->write_vio.cont == nullptr || this->write_vio.cont->mutex == nullptr || this->write_vio.op == VIO::NONE ||
+      this->write_vio.nbytes == 0) {
     return;
   }
 
@@ -695,7 +696,7 @@ Http2Stream::signal_write_event(bool call_update)
     return;
   }
 
-  if (this->write_vio.get_writer()->write_avail() == 0) {
+  if (this->write_vio.get_writer()->write_avail() == 0 || this->write_vio.nbytes == 0) {
     return;
   }
 
