8d5deb31d271cbd625d5ada5b5d4d1fc9b19740d
Susan Hinrichs <shinrich@verizonmedia.com>
2020-11-09 17:10:25 +0000

    Checkpoint, h2 session reuse and inactivity timeout

--- proxy/http2/Http2Stream.cc
+++ proxy/http2/Http2Stream.cc
@@ -383,7 +383,12 @@ Http2Stream::do_io_close(int /* flags */)
     _clear_timers();
     clear_io_events();
 
-    // Wait until transaction_done is called from HttpSM to signal that the TXN_CLOSE hook has been executed
+    // If this is a server transaction, just take it down.
+    if (this->is_outbound_connection()) {
+      this->transaction_done();
+    }
+
+    // Otherwise, Wait until transaction_done is called from HttpSM to signal that the TXN_CLOSE hook has been executed
   }
 }
 
