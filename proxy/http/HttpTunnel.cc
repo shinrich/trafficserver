e279fe230ad5d30ae6c33c30ea95252875d17652
Susan Hinrichs <shinrich@verizonmedia.com>
2020-11-30 21:19:50 +0000

    Checkpoint

--- proxy/http/HttpTunnel.cc
+++ proxy/http/HttpTunnel.cc
@@ -1191,6 +1191,9 @@ HttpTunnel::producer_handler(int event, HttpTunnelProducer *p)
     // Kick off the consumers if appropriate
     for (c = p->consumer_list.head; c; c = c->link.next) {
       if (c->alive && c->write_vio) {
+        if (c->write_vio->nbytes == INT64_MAX) {
+          c->write_vio->nbytes = p->bytes_read + p->init_bytes_done;
+        }
         c->write_vio->reenable();
       }
     }
