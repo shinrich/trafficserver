265ca1e4083db8d83c02030c1fdd4ae0b3d7036f
Susan Hinrichs <shinrich@oath.com>
2021-04-02 17:55:37 -0500

    Add class to normalize handling of pending action (#416)
    (cherry picked from commit 7cce49ec87bfb0327c96ba58a29c845a716e9b9a)

--- proxy/http/HttpSM.cc
+++ proxy/http/HttpSM.cc
@@ -1484,9 +1484,6 @@ HttpSM::state_api_callout(int event, void *data)
   // This is a reschedule via the tunnel.  Just fall through
   //
   case EVENT_INTERVAL:
-    if (data != pending_action) {
-      pending_action->cancel();
-    }
     pending_action = nullptr;
   // FALLTHROUGH
   case EVENT_NONE:
@@ -1537,7 +1534,7 @@ plugins required to work with sni_routing.
       if (!lock.is_locked()) {
         api_timer = -Thread::get_hrtime_updated();
         HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::state_api_callout);
-        ink_assert(pending_action == nullptr);
+        ink_release_assert(pending_action == nullptr);
         pending_action = mutex->thread_holding->schedule_in(this, HRTIME_MSECONDS(10));
         return -1;
       }
@@ -2359,12 +2356,8 @@ HttpSM::state_hostdb_lookup(int event, void *data)
     opt.timeout        = (t_state.api_txn_dns_timeout_value != -1) ? t_state.api_txn_dns_timeout_value : 0;
     opt.host_res_style = ats_host_res_from(ua_txn->get_netvc()->get_local_addr()->sa_family, t_state.txn_conf->host_res_data.order);
 
-    Action *dns_lookup_action_handle =
-      hostDBProcessor.getbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_hostdb_info, host_name, 0, opt);
-    if (dns_lookup_action_handle != ACTION_RESULT_DONE) {
-      ink_assert(!pending_action);
-      pending_action = dns_lookup_action_handle;
-    } else {
+    pending_action = hostDBProcessor.getbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_hostdb_info, host_name, 0, opt);
+    if (pending_action == nullptr) {
       call_transact_and_set_next_state(nullptr);
     }
   } break;
@@ -2498,13 +2491,13 @@ HttpSM::state_cache_open_write(int event, void *data)
 
   // Make sure we are on the "right" thread
   if (ua_txn) {
-    if (pending_action) {
-      pending_action->cancel();
-    }
-    if ((pending_action = ua_txn->adjust_thread(this, event, data))) {
+    pending_action = ua_txn->adjust_thread(this, event, data);
+    if (pending_action != nullptr) {
       HTTP_INCREMENT_DYN_STAT(http_cache_open_write_adjust_thread_stat);
       return 0; // Go away if we reschedule
     }
+    NetVConnection *vc = ua_txn->get_netvc();
+    ink_release_assert(vc && vc->thread == this_ethread());
   }
 
   milestones[TS_MILESTONE_CACHE_OPEN_WRITE_END] = Thread::get_hrtime();
@@ -4185,13 +4178,7 @@ HttpSM::do_remap_request(bool run_inline)
   }
 
   SMDebug("url_rewrite", "Found a remap map entry for [%" PRId64 "], attempting to remap request and call any plugins", sm_id);
-  Action *remap_action_handle = remapProcessor.perform_remap(this, &t_state);
-
-  if (remap_action_handle != ACTION_RESULT_DONE) {
-    SMDebug("url_rewrite", "Still more remapping needed for [%" PRId64 "]", sm_id);
-    ink_assert(!pending_action);
-    pending_action = remap_action_handle;
-  }
+  pending_action = remapProcessor.perform_remap(this, &t_state);
 
   return;
 }
@@ -4217,13 +4204,8 @@ HttpSM::do_hostdb_lookup()
     if (t_state.api_txn_dns_timeout_value != -1) {
       opt.timeout = t_state.api_txn_dns_timeout_value;
     }
-    Action *srv_lookup_action_handle =
-      hostDBProcessor.getSRVbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_srv_info, d, 0, opt);
-
-    if (srv_lookup_action_handle != ACTION_RESULT_DONE) {
-      ink_assert(!pending_action);
-      pending_action = srv_lookup_action_handle;
-    } else {
+    pending_action = hostDBProcessor.getSRVbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_srv_info, d, 0, opt);
+    if (pending_action == nullptr) {
       char *host_name = t_state.dns_info.srv_lookup_success ? t_state.dns_info.srv_hostname : t_state.dns_info.lookup_name;
       opt.port        = t_state.dns_info.srv_lookup_success ?
                    t_state.dns_info.srv_port :
@@ -4235,12 +4217,8 @@ HttpSM::do_hostdb_lookup()
       opt.host_res_style =
         ats_host_res_from(ua_txn->get_netvc()->get_local_addr()->sa_family, t_state.txn_conf->host_res_data.order);
 
-      Action *dns_lookup_action_handle =
-        hostDBProcessor.getbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_hostdb_info, host_name, 0, opt);
-      if (dns_lookup_action_handle != ACTION_RESULT_DONE) {
-        ink_assert(!pending_action);
-        pending_action = dns_lookup_action_handle;
-      } else {
+      pending_action = hostDBProcessor.getbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_hostdb_info, host_name, 0, opt);
+      if (pending_action == nullptr) {
         call_transact_and_set_next_state(nullptr);
       }
     }
@@ -4271,13 +4249,9 @@ HttpSM::do_hostdb_lookup()
 
     opt.host_res_style = ats_host_res_from(ua_txn->get_netvc()->get_local_addr()->sa_family, t_state.txn_conf->host_res_data.order);
 
-    Action *dns_lookup_action_handle = hostDBProcessor.getbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_hostdb_info,
-                                                                     t_state.dns_info.lookup_name, 0, opt);
-
-    if (dns_lookup_action_handle != ACTION_RESULT_DONE) {
-      ink_assert(!pending_action);
-      pending_action = dns_lookup_action_handle;
-    } else {
+    pending_action = hostDBProcessor.getbyname_imm(this, (cb_process_result_pfn)&HttpSM::process_hostdb_info,
+                                                   t_state.dns_info.lookup_name, 0, opt);
+    if (pending_action == nullptr) {
       call_transact_and_set_next_state(nullptr);
     }
     return;
@@ -4296,12 +4270,8 @@ HttpSM::do_hostdb_reverse_lookup()
 
   IpEndpoint addr;
   ats_ip_pton(t_state.dns_info.lookup_name, &addr.sa);
-  Action *dns_lookup_action_handle = hostDBProcessor.getbyaddr_re(this, &addr.sa);
+  pending_action = hostDBProcessor.getbyaddr_re(this, &addr.sa);
 
-  if (dns_lookup_action_handle != ACTION_RESULT_DONE) {
-    ink_assert(!pending_action);
-    pending_action = dns_lookup_action_handle;
-  }
   return;
 }
 
@@ -4710,7 +4680,7 @@ HttpSM::do_cache_lookup_and_read()
   HttpCacheKey key;
   Cache::generate_key(&key, c_url, t_state.txn_conf->cache_generation_number);
 
-  Action *cache_action_handle = cache_sm.open_read(
+  pending_action = cache_sm.open_read(
     &key, c_url, &t_state.hdr_info.client_request, t_state.txn_conf,
     static_cast<time_t>((t_state.cache_control.pin_in_cache_for < 0) ? 0 : t_state.cache_control.pin_in_cache_for));
   //
@@ -4719,11 +4689,7 @@ HttpSM::do_cache_lookup_and_read()
   // optimize the typical open_read/open_read failed/open_write
   // sequence.
   //
-  if (cache_action_handle != ACTION_RESULT_DONE) {
-    ink_assert(!pending_action);
-    pending_action = cache_action_handle;
-  }
-  REMEMBER((long)pending_action, reentrancy_count);
+  REMEMBER((long)pending_action.get(), reentrancy_count);
 
   return;
 }
@@ -4737,17 +4703,9 @@ HttpSM::do_cache_delete_all_alts(Continuation *cont)
   SMDebug("http_seq", "[HttpSM::do_cache_delete_all_alts] Issuing cache delete for %s",
           t_state.cache_info.lookup_url->string_get_ref());
 
-  Action *cache_action_handle = nullptr;
-
   HttpCacheKey key;
   Cache::generate_key(&key, t_state.cache_info.lookup_url, t_state.txn_conf->cache_generation_number);
-  cache_action_handle = cacheProcessor.remove(cont, &key);
-  if (cont != nullptr) {
-    if (cache_action_handle != ACTION_RESULT_DONE) {
-      ink_assert(!pending_action);
-      pending_action = cache_action_handle;
-    }
-  }
+  pending_action = cacheProcessor.remove(cont, &key);
 
   return;
 }
@@ -4797,7 +4755,7 @@ HttpSM::do_cache_prepare_action(HttpCacheSM *c_sm, CacheHTTPInfo *object_read_in
   URL *o_url, *s_url;
   bool restore_client_request = false;
 
-  ink_assert(!pending_action);
+  ink_assert(pending_action == nullptr);
 
   if (t_state.redirect_info.redirect_in_process) {
     o_url = &(t_state.redirect_info.original_url);
@@ -4826,15 +4784,10 @@ HttpSM::do_cache_prepare_action(HttpCacheSM *c_sm, CacheHTTPInfo *object_read_in
   HttpCacheKey key;
   Cache::generate_key(&key, s_url, t_state.txn_conf->cache_generation_number);
 
-  Action *cache_action_handle =
+  pending_action =
     c_sm->open_write(&key, s_url, &t_state.hdr_info.client_request, object_read_info,
                      static_cast<time_t>((t_state.cache_control.pin_in_cache_for < 0) ? 0 : t_state.cache_control.pin_in_cache_for),
                      retry, allow_multiple);
-
-  if (cache_action_handle != ACTION_RESULT_DONE) {
-    ink_assert(!pending_action);
-    pending_action = cache_action_handle;
-  }
 }
 
 void
@@ -4934,15 +4887,9 @@ HttpSM::do_http_server_open(bool raw)
   auto fam_name = ats_ip_family_name(ip_family);
   SMDebug("http_track", "entered inside do_http_server_open ][%.*s]", static_cast<int>(fam_name.size()), fam_name.data());
 
-  // Make sure we are on the "right" thread
-  if (ua_txn) {
-    if ((pending_action = ua_txn->adjust_thread(this, EVENT_INTERVAL, nullptr))) {
-      HTTP_INCREMENT_DYN_STAT(http_origin_connect_adjust_thread_stat);
-      return; // Go away if we reschedule
-    }
-  }
+  NetVConnection *vc = ua_txn->get_netvc();
+  ink_release_assert(vc && vc->thread == this_ethread());
   pending_action = nullptr;
-  ink_assert(server_entry == nullptr);
 
   // Clean up connection tracking info if any. Need to do it now so the selected group
   // is consistent with the actual upstream in case of retry.
@@ -5209,8 +5156,6 @@ HttpSM::do_http_server_open(bool raw)
   }
 
   // We did not manage to get an existing session and need to open a new connection
-  Action *connect_action_handle;
-
   NetVCOptions opt;
   opt.f_blocking_connect = false;
   opt.set_sock_param(t_state.txn_conf->sock_recv_buffer_size_out, t_state.txn_conf->sock_send_buffer_size_out,
@@ -5303,19 +5248,14 @@ HttpSM::do_http_server_open(bool raw)
       opt.set_ssl_servername(t_state.server_info.name);
     }
 
-    connect_action_handle = sslNetProcessor.connect_re(this,                                 // state machine
-                                                       &t_state.current.server->dst_addr.sa, // addr + port
-                                                       &opt);
+    pending_action = sslNetProcessor.connect_re(this,                                 // state machine
+                                                &t_state.current.server->dst_addr.sa, // addr + port
+                                                &opt);
   } else {
     SMDebug("http", "calling netProcessor.connect_re");
-    connect_action_handle = netProcessor.connect_re(this,                                 // state machine
-                                                    &t_state.current.server->dst_addr.sa, // addr + port
-                                                    &opt);
-  }
-
-  if (connect_action_handle != ACTION_RESULT_DONE) {
-    ink_assert(!pending_action);
-    pending_action = connect_action_handle;
+    pending_action = netProcessor.connect_re(this,                                 // state machine
+                                             &t_state.current.server->dst_addr.sa, // addr + port
+                                             &opt);
   }
 
   return;
@@ -7065,10 +7005,9 @@ HttpSM::kill_this()
     // state. This is because we are depending on the
     // callout to complete for the state machine to
     // get killed.
-    if (callout_state == HTTP_API_NO_CALLOUT && pending_action) {
-      pending_action->cancel();
+    if (callout_state == HTTP_API_NO_CALLOUT && pending_action != nullptr) {
       pending_action = nullptr;
-    } else if (pending_action) {
+    } else if (pending_action != nullptr) {
       ink_assert(pending_action == nullptr);
     }
 
@@ -7131,10 +7070,7 @@ HttpSM::kill_this()
   //   then the value of kill_this_async_done has changed so
   //   we must check it again
   if (kill_this_async_done == true) {
-    if (pending_action) {
-      pending_action->cancel();
-      pending_action = nullptr;
-    }
+    pending_action = nullptr;
     if (t_state.http_config_param->enable_http_stats) {
       update_stats();
     }
@@ -7765,17 +7701,10 @@ HttpSM::set_next_state()
     break;
   }
 
-  case HttpTransact::SM_ACTION_INTERNAL_REQUEST: {
+  case HttpTransact::SM_ACTION_INTERNAL_REQUEST:
     HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::state_handle_stat_page);
-    Action *action_handle = statPagesManager.handle_http(this, &t_state.hdr_info.client_request);
-
-    if (action_handle != ACTION_RESULT_DONE) {
-      ink_assert(pending_action == nullptr);
-      pending_action = action_handle;
-    }
-
+    pending_action = statPagesManager.handle_http(this, &t_state.hdr_info.client_request);
     break;
-  }
 
   case HttpTransact::SM_ACTION_ORIGIN_SERVER_RR_MARK_DOWN: {
     HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::state_mark_os_down);
