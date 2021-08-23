1c6674c324f125c7bf9fcd2b5679f0467352d556
Brian Neradt <brian.neradt@verizonmedia.com>
2021-08-23 16:31:57 -0500

    Locking around SSLSecret::secret_map access (#530)
    Fixes some crashes from corrupted iterators for SSLSecret::secret_map by
    making access to them exclusive.

--- iocore/net/SSLSecret.cc
+++ iocore/net/SSLSecret.cc
@@ -71,6 +71,7 @@ SSLSecret::loadFile(const std::string &name, std::string &data_item)
 bool
 SSLSecret::setSecret(const std::string &name, const char *data, int data_len)
 {
+  std::scoped_lock _(secret_map_mutex);
   auto iter = secret_map.find(name);
   if (iter == secret_map.end()) {
     secret_map[name] = "";
@@ -87,6 +88,7 @@ SSLSecret::setSecret(const std::string &name, const char *data, int data_len)
 const std::string *
 SSLSecret::getSecretItem(const std::string &name) const
 {
+  std::scoped_lock _(secret_map_mutex);
   auto iter = secret_map.find(name);
   if (iter == secret_map.end()) {
     return nullptr;
@@ -110,6 +112,7 @@ SSLSecret::getSecret(const std::string &name, std::string_view &data) const
 bool
 SSLSecret::getOrLoadSecret(const std::string &name1, const std::string &name2, std::string_view &data1, std::string_view &data2)
 {
+  std::scoped_lock _(secret_map_mutex);
   bool found_secret1 = this->getSecret(name1, data1);
   bool found_secret2 = name2.empty() || this->getSecret(name2, data2);
 
